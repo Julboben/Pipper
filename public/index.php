4c9dc826ddff34418ca8c4b6da5cdb17f330e6f2
Julian Bendtsen <91532158+Julboben@users.noreply.github.com>
2022-09-13 12:12:40 +0200

    Virker

--- public/index.php
+++ public/index.php
@@ -9,7 +9,7 @@ header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-/* echo "Hello world"; */
+// echo "Hello world";
 
 // Runs local server!
 // php -S 127.0.0.1:8000 -t public​
@@ -18,18 +18,46 @@ $servername = getenv("SERVERNAME");
 $username = getenv("USERNAME");
 $password = getenv("PASSWORD");
 
+$uri = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+$uri = explode( "/", $uri );
+// echo $uri[1];
+
+$requestType = $_SERVER["REQUEST_METHOD"];
+// echo $requestType;
+
 try {
   $conn = new PDO("mysql:host=$servername;dbname=pipper", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   // echo "Connected successfully! ";
 
-  $statement = $conn->query("SELECT * FROM pip;");
-  $result = $statement->fetchAll(\PDO::FETCH_ASSOC);
-
-  echo json_encode($result);  
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
 
-?> 
+if ($requestType == "GET") {
+  $statement = $conn->query("SELECT * FROM pipper.pip INNER JOIN pipper.users ON pipper.users.username = pipper.pip.userpip ORDER BY timepip DESC;");
+  $result = $statement->fetchAll(\PDO::FETCH_ASSOC);
+
+  echo json_encode($result);
+} elseif($requestType == "POST") {
+    $input = (array) json_decode(file_get_contents("php://input"), TRUE);
+    //echo $input['textpip'];
+    $statement = "INSERT INTO pipper.pip (idpip, textpip, userpip, timepip, likespip) VALUES (default, :textpip, :username, :timepip, '0')";
+
+    try {
+      $statement = $conn->prepare($statement);
+      $statement->execute(array("textpip" => $input['textpip'], "username" =>  $input['username'], "timepip" =>  $input['timepip']));
+
+      echo json_encode("The pip has been send!");
+
+
+    } catch(PDOException $e) {
+      echo json_encode("The Pip has not been received!") . $e->getMessage();
+    }
+
+} elseif($requestType == "PUT") {
+  $input = (array) json_decode(file_get_contents("php://input"), TRUE);
+}
+
+?>
--- scripts.js
+++ scripts.js
@@ -1,10 +1,20 @@
 // Text input counter
-var maxchar = 255;
-var i = document.getElementById("textpip");
-var c = document.getElementById("count");
+/* const maxchar = 255;
+const i = document.getElementById("textPip");
+const c = document.getElementById("count");
 c.innerHTML = 0;
 
-i.addEventListener("keydown", count);
+i.addEventListener("keydown", count); */
+
+const localHost = "http://localhost:8000";
+
+document.getElementById("textPip").addEventListener("keyup", (count) => {
+   const counter = document.getElementById("count");
+   const length = document.getElementById("textPip").value.length;
+   counter.innerHTML = length;
+})
+
+
 
 // Form
 function count(e) {
@@ -19,7 +29,7 @@ function count(e) {
 // Fetches database on LOAD
 window.addEventListener("load", async () => {
    const response = await fetch(
-      "http://localhost:8000"
+      localHost
    );
    console.log(response)
 
@@ -36,18 +46,25 @@ window.addEventListener("load", async () => {
    }
 
    items.forEach(item => {
-      console.log(item);
+      //console.log(item);
 
       const printPostedPips = `<div class="pipFeed">
       <div class="postedPipBox">
-       <div class="postedPipBoxContent d-flex">
-        <img class="userImg" src="/img/profilepic.jpg" alt="">
-        <div>
-            <div class="username"><span>${item.userpip}</span></div>
+       <div class="postedPipBoxContent">
+       <div class="postedPipImg"><img class="userImg" src="${item.userimage}" alt="profilepic"></div>
+        <div class="postedPipTextContent">
+            <div class="userInfo"><span class="userfirstname">${item.userfirstname}</span><span><svg class="authenticate-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-patch-check-fill" viewBox="0 0 16 16">
+            <path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708z"/>
+          </svg></span><span class="username">${item.userpip}</span></div>
             <div class="postedPipDate"><span>${item.timepip}</span></div>
             <div class="postedPipText">
                 <p>${item.textpip}</p>
                  </div>
+                 <div class="postedPipActions row row-cols-2"><div class="col"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-left" viewBox="0 0 16 16">
+                 <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4.414A2 2 0 0 0 3 11.586l-2 2V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+               </svg></div><div class="col"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+               <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+             </svg>&nbsp${item.likespip}</div></div>
                </div>
            </div>
          </div>
@@ -55,29 +72,46 @@ window.addEventListener("load", async () => {
 
       const div = document.querySelector(".pipFeed");
       div.innerHTML += printPostedPips;
-
    });
 
-
+   // Hvad foregår der her??? VI har "måske" fat i noget
+   document.getElementById("userPip").innerHTML = `<div>${item.userimage}</div>`;
 });
 
-/* const printPostedPips = `<div class="pipFeed">
-<div class="postedPipBox">
-    <div class="postedPipBoxContent d-flex">
-        <img class="userImg" src="/img/profilepic.jpg" alt="">
-        <div>
-            <div class="username"><span>${pip.name}</span></div>
-            <div class="postedPipDate"><span>${pip.date}</span></div>
-            <div class="postedPipText">
-                <p>${pip.text}</p>
-            </div>
-        </div>
-    </div>
-</div>
-</div>`;
+// POST TO SQL
 
-const div = document.querySelector(".pipFeed");
-div.innerHTML += printPostedPips; */
+postPipForm = document.querySelector("#postPipForm");
+
+postPipForm.addEventListener("submit", async (event) => {
+   event.preventDefault();
+
+   date = new Date().toISOString().slice(0, 19).replace('T', ' ');
+   const textPipValue = document.getElementById("textPip").value;
+
+   const rawResponse = await fetch(localHost, {
+      method: 'POST',
+      body: JSON.stringify({ textpip: textPipValue, username: "Thavlov", timepip: date })
+   });
+   console.log(rawResponse);
+   const content = await rawResponse.json();
+
+   console.log(content);
+
+
+
+   // alert("Din besked er sendt kammerat!");
+
+})
+
+/*    fillTemplate(titel, note) */
+/* $(function() {
+   $('#myForm').submit(function(){
+      var query = $(this).serialize();
+      $.post("post.php", query);
+      return false;
+  });
+});
+ */
 
 // Modal
 
@@ -85,16 +119,16 @@ const pipBtnModal = document.getElementById("pipOpenModal");
 const pipCloseModal = document.getElementById("pipCloseModal");
 const modal = document.getElementById("pipModal");
 
-pipBtnModal.addEventListener("click", function() {
+pipBtnModal.addEventListener("click", function () {
    modal.style.display = "block";
 });
 
-pipCloseModal.addEventListener("click", function() {
+pipCloseModal.addEventListener("click", function () {
    modal.style.display = "none";
 });
 
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+window.onclick = function (event) {
+   if (event.target == modal) {
+      modal.style.display = "none";
+   }
 }
